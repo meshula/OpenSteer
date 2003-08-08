6a1e4a8329cb37b1311901deb8d93e0a7e66b6e3
cwreynolds <cwreynolds@963f2c78-931d-0410-aaae-9c88778f2814>
2003-08-08 22:53:08 +0000

    use a spatial database to accelerate proximity queries

--- opensteer/plugins/Boids.cpp
+++ opensteer/plugins/Boids.cpp
@@ -36,8 +36,17 @@
 // ----------------------------------------------------------------------------
 
 
+#include <sstream>
 #include "OpenSteer/SimpleVehicle.h"
 #include "OpenSteer/SteerTest.h"
+#include "OpenSteer/Proximity.h"
+
+
+// ----------------------------------------------------------------------------
+
+
+typedef AbstractProximityDatabase<AbstractVehicle*> ProximityDatabase;
+typedef AbstractTokenForProximityDatabase<AbstractVehicle*> ProximityToken;
 
 
 // ----------------------------------------------------------------------------
@@ -53,9 +62,28 @@ public:
     // per-boid reference to its flock
     const groupType& flock;
 
-    // constructor: argument is a reference to a flock container which is 
-    // still being filled with the newly constructed member of the flock
-    Boid (const groupType& FLOCK) : flock (FLOCK) {reset ();}
+    // a pointer to this boid's interface object for the proximity database
+    ProximityToken* proximityToken;
+
+    // constructor: argument is a reference to a flock container which is
+    // still being filled with the newly constructed member of the flock (that
+    // container is now redundant since the ProximityDatabase is holding the
+    // relvant information, the flock argument needs to be removed.)
+    Boid (const groupType& FLOCK, ProximityDatabase& pd) : flock (FLOCK)
+    {
+        // allocate a token for this boid in the proximity database
+        proximityToken = pd.allocateToken (this);
+
+        // reset all boid state
+        reset ();
+    }
+
+    // destructor
+    ~Boid ()
+    {
+        // delete this boid's token in the proximity database
+        delete proximityToken;
+    }
 
     // reset state
     void reset (void)
@@ -63,14 +91,14 @@ public:
         // reset the vehicle
         SimpleVehicle::reset ();
 
-        // initial speed
-        setSpeed (0);
-
         // steering force is clipped to this magnitude
         setMaxForce (27);
 
         // velocity is clipped to this magnitude
         setMaxSpeed (9);
+
+        // initial slow speed
+        setSpeed (maxSpeed() * 0.3);
 
         // randomize initial orientation
         regenerateOrthonormalBasisUF (RandomUnitVector ());
@@ -106,7 +134,11 @@ public:
             const Vec3 lateral = seek.perpendicularComponent (forward ());
             applySteeringForce (flocking + lateral, elapsedTime);
         }
+
+        // notify proximity database that our position has changed
+        proximityToken->updateForNewPosition (position());
     }
+
 
     // basic flocking
     Vec3 steerToFlock (void)
@@ -123,18 +155,26 @@ public:
         const float cohesionAngle  = -0.15f;
         const float cohesionWeight = 8.0f;
 
-        const AVGroup& avflock = (AVGroup&)flock;
+        const float maxRadius = std::max (separationRadius,
+                                          std::max (alignmentRadius,
+                                                    cohesionRadius));
 
+        // find all flockmates within maxRadius using proximity database
+        neighbors.clear();
+        proximityToken->findNeighbors (position(), maxRadius, neighbors);
+
+        // determine each of the three component behaviors of flocking
         const Vec3 separation = steerForSeparation (separationRadius,
                                                     separationAngle,
-                                                    avflock);
-        const Vec3 alignment = steerForAlignment (alignmentRadius,
-                                                  alignmentAngle,
-                                                  avflock);
-        const Vec3 cohesion = steerForCohesion (cohesionRadius,
-                                                cohesionAngle,
-                                                avflock);
+                                                    neighbors);
+        const Vec3 alignment  = steerForAlignment  (alignmentRadius,
+                                                    alignmentAngle,
+                                                    neighbors);
+        const Vec3 cohesion   = steerForCohesion   (cohesionRadius,
+                                                    cohesionAngle,
+                                                    neighbors);
 
+        // apply weights to components (save in variables for annotation)
         const Vec3 separationW = separation * separationWeight;
         const Vec3 alignmentW = alignment * alignmentWeight;
         const Vec3 cohesionW = cohesion * cohesionWeight;
@@ -148,13 +188,21 @@ public:
         return separationW + alignmentW + cohesionW;
     }
 
+
     // make boids "bank" as they fly
     void regenerateLocalSpace (const Vec3& newVelocity,
                                const float elapsedTime)
     {
         regenerateLocalSpaceForBanking (newVelocity, elapsedTime);
     }
+
+    // allocate one and share amoung instances just to save memory usage
+    // (change to per-instance allocation to be more mp-safe)
+    static AVGroup neighbors;
 };
+
+
+AVGroup Boid::neighbors;
 
 
 // ----------------------------------------------------------------------------
@@ -164,9 +212,6 @@ public:
 class BoidsPlugIn : public PlugIn
 {
 public:
-// -------------------------------------------------- xxxBasicProximity
-//     bool requestInitialSelection (void) {return true;}
-// -------------------------------------------------- xxxBasicProximity
     
     const char* name (void) {return "Boids";}
 
@@ -176,6 +221,14 @@ public:
 
     void open (void)
     {
+        // make the database used to accelerate proximity queries
+        // XXX the sphere boundary diameter is 100 and they frequently
+        // XXX go outside that.  Using 110, but need to link constants
+        // XXX here and in Boid::update
+        pd = new LQProximityDatabase<AbstractVehicle*> (Vec3::zero,
+                                                        Vec3 (110, 110, 110),
+                                                        Vec3 (15, 15, 15));
+        // make default-sized flock
         makeFlock (200);
 
         // initialize camera
@@ -217,6 +270,17 @@ public:
 
         // highlight selected vehicle
         SteerTest::drawCircleHighlightOnVehicle (selected, 1, gGray50);
+
+        // display status in the upper left corner of the window
+        std::ostringstream status;
+        status << "Flock size: " << population << std::ends;
+        const float h = drawGetWindowHeight ();
+        const Vec3 screenLocation (10, h-50, 0);
+        draw2dTextAt2dLocation (status, screenLocation, gGray80);
+        // std::ostringstream status2;
+        // status2  << "PD content count: " << pd->getPopulation ()<<std::endl;
+        // const Vec3 screenLocation2 (10, h-70, 0);
+        // draw2dTextAt2dLocation (status2, screenLocation2, gGray80);
     }
 
     void close (void)
@@ -224,6 +288,10 @@ public:
         // delete each member of the flock
         for (iterator i = flock.begin(); i != flock.end(); i++) delete (*i);
         flock.clear ();
+
+        // delete the proximity database
+        delete pd;
+        pd = NULL;
     }
 
     void reset (void)
@@ -238,14 +306,57 @@ public:
         SteerTest::camera.doNotSmoothNextMove ();
     }
 
-    const AVGroup& allVehicles (void) {return (const AVGroup&)flock;}
+    void handleFunctionKeys (int keyNumber)
+    {
+        switch (keyNumber)
+        {
+        case 1:
+            addBoidToFlock ();
+            break;
+        case 2:
+            if (population > 0) removeBoidFromFlock ();
+            break;
+        }
+    }
+
+    void printMiniHelpForFunctionKeys (void)
+    {
+        std::ostringstream message;
+        message << "Function keys handled by ";
+        message << '"' << name() << '"' << ':' << std::ends;
+        SteerTest::printMessage (message);
+        SteerTest::printMessage ("  F1     add a boid to the flock.");
+        SteerTest::printMessage ("  F2     remove a boid from the flock.");
+        SteerTest::printMessage ("");
+    }
+
+    void addBoidToFlock (void)
+    {
+        population++;
+        Boid* boid = new Boid (flock, *pd);
+        flock.push_back (boid);
+        if (population == 1) SteerTest::selectedVehicle = boid;
+    }
+
+    void removeBoidFromFlock (void)
+    {
+        // save a pointer to the last boid, then remove it from the flock
+        const Boid* boid = flock.back();
+        flock.pop_back();
+        population--;
+
+        // if it is SteerTest's selected vehicle, unselect it
+        if (boid == SteerTest::selectedVehicle)
+            SteerTest::selectedVehicle = NULL;
+
+        // delete the Boid
+        delete boid;
+    }
 
     // create new flock of the given size
-    void makeFlock (const int population)
+    void makeFlock (const int size)
     {
-        for (int i = 0; i < population; i++)
-            flock.push_back (new Boid (flock));
-        initializeFlock ();
+        for (int i = 0; i < size; i++) addBoidToFlock ();
     }
 
     // initialize the flock's positions and orientations
@@ -254,9 +365,18 @@ public:
         for (iterator i = flock.begin(); i != flock.end(); i++) (**i).reset();
     }
 
+    // return an AVGroup containing each boid of the flock
+    const AVGroup& allVehicles (void) {return (const AVGroup&)flock;}
+
     // flock: a group (STL vector) of pointers to all boids
     Boid::groupType flock;
     typedef Boid::groupType::const_iterator iterator;
+
+    // pointer to database used to accelerate proximity queries
+    ProximityDatabase* pd;
+
+    // keep track of current flock size
+    int population;
 };
 
 
