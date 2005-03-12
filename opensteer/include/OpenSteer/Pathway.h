3d3e56db33a62c5bb523204e7d50da2e7c1ad368
meshula <meshula@963f2c78-931d-0410-aaae-9c88778f2814>
2005-03-12 23:13:04 +0000

    allow pathpoints to be updated dynamically

--- opensteer/include/OpenSteer/Pathway.h
+++ opensteer/include/OpenSteer/Pathway.h
@@ -51,39 +51,42 @@ namespace OpenSteer {
     // example would be used in path following.
 
 
-    class Pathway
-    {
-    public:
-        // Given an arbitrary point ("A"), returns the nearest point ("P") on
-        // this path.  Also returns, via output arguments, the path tangent at
-        // P and a measure of how far A is outside the Pathway's "tube".  Note
-        // that a negative distance indicates A is inside the Pathway.
-        virtual Vec3 mapPointToPath (const Vec3& point,
-                                     Vec3& tangent,
-                                     float& outside) = 0;
+	class Pathway
+	{
+	public:
+		Pathway() { }
+		virtual ~Pathway() { }
 
-        // given a distance along the path, convert it to a point on the path
-        virtual Vec3 mapPathDistanceToPoint (float pathDistance) = 0;
+		// Given an arbitrary point ("A"), returns the nearest point ("P") on
+		// this path.  Also returns, via output arguments, the path tangent at
+		// P and a measure of how far A is outside the Pathway's "tube".  Note
+		// that a negative distance indicates A is inside the Pathway.
+		virtual Vec3 mapPointToPath (const Vec3& point,
+			Vec3& tangent,
+			float& outside) = 0;
 
-        // Given an arbitrary point, convert it to a distance along the path.
-        virtual float mapPointToPathDistance (const Vec3& point) = 0;
+		// given a distance along the path, convert it to a point on the path
+		virtual Vec3 mapPathDistanceToPoint (float pathDistance) = 0;
 
-        // is the given point inside the path tube?
-        bool isInsidePath (const Vec3& point)
-        {
-            float outside; Vec3 tangent;
-            mapPointToPath (point, tangent, outside);
-            return outside < 0;
-        }
+		// Given an arbitrary point, convert it to a distance along the path.
+		virtual float mapPointToPathDistance (const Vec3& point) = 0;
 
-        // how far outside path tube is the given point?  (negative is inside)
-        float howFarOutsidePath (const Vec3& point)
-        {
-            float outside; Vec3 tangent;
-            mapPointToPath (point, tangent, outside);
-            return outside;
-        }
-    };
+		// is the given point inside the path tube?
+		bool isInsidePath (const Vec3& point)
+		{
+			float outside; Vec3 tangent;
+			mapPointToPath (point, tangent, outside);
+			return outside < 0;
+		}
+
+		// how far outside path tube is the given point?  (negative is inside)
+		float howFarOutsidePath (const Vec3& point)
+		{
+			float outside; Vec3 tangent;
+			mapPointToPath (point, tangent, outside);
+			return outside;
+		}
+	};
 
 
     // ----------------------------------------------------------------------------
@@ -93,70 +96,80 @@ namespace OpenSteer {
     // point and a cylinder along each segment.
 
 
-    class PolylinePathway: public virtual Pathway
-    {
-    public:
+	class PolylinePathway: public virtual Pathway
+	{
+	public:
 
-        int pointCount;
-        Vec3* points;
-        float radius;
-        bool cyclic;
+		int pointCount;
+		Vec3* points;
+		float radius;
+		bool cyclic;
 
-        PolylinePathway (void) {}
+		PolylinePathway (void) {}
+		virtual ~PolylinePathway() { }
 
-        // construct a PolylinePathway given the number of points (vertices),
-        // an array of points, and a path radius.
-        PolylinePathway (const int _pointCount,
-                         const Vec3 _points[],
-                         const float _radius,
-                         const bool _cyclic);
+		// construct a PolylinePathway given the number of points (vertices),
+		// an array of points, and a path radius.
+		PolylinePathway (const int _pointCount,
+			const Vec3 _points[],
+			const float _radius,
+			const bool _cyclic);
+
+		// utility for constructors in derived classes
+		void initialize (const int _pointCount,
+			const Vec3 _points[],
+			const float _radius,
+			const bool _cyclic);
 
         // utility for constructors in derived classes
-        void initialize (const int _pointCount,
-                         const Vec3 _points[],
-                         const float _radius,
-                         const bool _cyclic);
+		void 
+		setupLengths ();
 
-        // Given an arbitrary point ("A"), returns the nearest point ("P") on
+        // move existing points safely
+        void movePoints (const int _firstPoint,
+						const int _numPoints,
+                        const Vec3 _points[]);
+
+		// Given an arbitrary point ("A"), returns the nearest point ("P") on
         // this path.  Also returns, via output arguments, the path tangent at
         // P and a measure of how far A is outside the Pathway's "tube".  Note
         // that a negative distance indicates A is inside the Pathway.
         Vec3 mapPointToPath (const Vec3& point, Vec3& tangent, float& outside);
 
 
-        // given an arbitrary point, convert it to a distance along the path
-        float mapPointToPathDistance (const Vec3& point);
+		// given an arbitrary point, convert it to a distance along the path
+		float mapPointToPathDistance (const Vec3& point);
 
-        // given a distance along the path, convert it to a point on the path
-        Vec3 mapPathDistanceToPoint (float pathDistance);
+		// given a distance along the path, convert it to a point on the path
+		Vec3 mapPathDistanceToPoint (float pathDistance);
 
-        // utility methods
+		// utility methods
 
-        // compute minimum distance from a point to a line segment
-        float pointToSegmentDistance (const Vec3& point,
-                                      const Vec3& ep0,
-                                      const Vec3& ep1);
+		// compute minimum distance from a point to a line segment
+		float pointToSegmentDistance (const Vec3& point,
+			const Vec3& ep0,
+			const Vec3& ep1);
 
-        // assessor for total path length;
-        float getTotalPathLength (void) {return totalPathLength;};
+		// assessor for total path length;
+		float getTotalPathLength (void) {return totalPathLength;};
 
-    // XXX removed the "private" because it interfered with derived
-    // XXX classes later this should all be rewritten and cleaned up
-    // private:
+		// XXX removed the "private" because it interfered with derived
+		// XXX classes later this should all be rewritten and cleaned up
+		// private:
 
-        // xxx shouldn't these 5 just be local variables?
-        // xxx or are they used to pass secret messages between calls?
-        // xxx seems like a bad design
-        float segmentLength;
-        float segmentProjection;
-        Vec3 local;
-        Vec3 chosen;
-        Vec3 segmentNormal;
+		// xxx shouldn't these 5 just be local variables?
+		// xxx or are they used to pass secret messages between calls?
+		// xxx seems like a bad design
+		float segmentLength;
+		float segmentProjection;
+		Vec3 local;
+		Vec3 chosen;
+		Vec3 segmentNormal;
 
-        float* lengths;
-        Vec3* normals;
-        float totalPathLength;
-    };
+		float* lengths;
+		Vec3* normals;
+		float totalPathLength;
+	};
 
 } // namespace OpenSteer
     
